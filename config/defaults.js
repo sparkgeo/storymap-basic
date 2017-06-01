23346ca594f452ddafdf7c789d6163d31dc4ebf0
Alain St. Pierre <alain@sparkgeo.com>
2017-06-01 14:43:59 -0700

    set appid for template

--- config/defaults.js
+++ config/defaults.js
@@ -18,7 +18,7 @@
 define({
   //Default configuration settings for the application. This is where you'll define things like a bing maps key,
   //default web map, default app color theme and more. These values can be overwritten by template configuration settings and url parameters.
-  "appid": "",
+  "appid": "f69089cef5164f8587c23ce73493f836",
   "webmap": "24e01ef45d40423f95300ad2abc5038a",
   "oauthappid": null, //"AFTKRmv16wj14N3z",
   //Group templates must support a group url parameter. This will contain the id of the group.
